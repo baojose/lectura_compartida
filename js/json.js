7395be12f4733d63df214e21fab7323079672be6
Dan <corneliusinlove@gmail.com>
2014-01-12 22:11:22 +0100

    copiado js y img desde dani/ a raiz. funciona book shelf. funciona js y jquery

---
+++ js/json.js
@@ -0,0 +1,38 @@
+var libros=
+{
+  "libro1": {
+    "titulo": "El Francotirador Paciente",
+    "descripcion": "La ciudad dddd es un campo de batalla. Un artista callejero lanza desafíos como si fueran bombas. El único arte posible es un ajuste de cuentas.",
+    "link_ebook": "http://link_ebook_libro1.com",
+    "link_comprar": "http://link_comprar_libro1.com",
+    "path_img": "img/book1.png",
+    "cajas":[
+      {"sobre_el_autor":{"text":"1111sñakfsdñ", "path_img":"toe"}},
+      {"sabias_que":{"text":"2222sasñakfsdñ", "path_img":"toe"}},
+      {"playlist":{"text":"3333s3ñakfsdñ", "path_img":"toe"}},
+      {"videos":{"text":"4444sñakfsdñ", "path_img":"toe"}}
+
+    ],
+    "array": [
+      "valor1",
+      "valor2",
+      "valor3",
+      "valor4",
+      "valor5"
+    ]
+  },
+  "libro2": {
+    "titulo": "Ad consequat mollit",
+    "descripcion": "2MMMMMM222S Lorem ipsum Ad consequat mollit proident veniam Ut quis aute sed sit ullamco commodo occaecat eiusmod commodo in exercitation.",
+    "link_ebook": "http://link_ebook_libro2.com",
+    "link_comprar": "http://link_comprar_libro2.com",
+    "path_img": "img/juil_dyler_la_unidad_nilog_al_sima_hongo_ajunjen.jpg",
+    "array": [
+      "valor1",
+      "valor2",
+      "valor3",
+      "valor4",
+      "valor5"
+    ]
+  }
+}
