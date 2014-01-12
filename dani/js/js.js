6ad3648528516db5c8425ea506d70ffe030e8336
Dan <corneliusinlove@gmail.com>
2014-01-12 16:49:03 +0100

    fadeOut y fadeIn de slider_content

--- dani/js/js.js
+++ dani/js/js.js
@@ -1,22 +1,35 @@
 $(document).ready(function() {
-  // TODO cargar la imagen landing
+  // cargar la imagen landing
+  cargarDatosPrincipales('libro1');
+
   console.log("jquery funcionando");
   // TODO borrar el contenido de todas las variables json. Sino el anterior valor sobreescribe un valor vacio
   
   // Detectar cual libro pequeño ha sido clicado, cambiar el contenido de slider_content
   $(".change_libros_peq img").click(function() {
+    // fade out slider_content
+    $(".slider_content").fadeOut();    
+    
     // detectar cual id ha sido clicado
     var id = $(this).attr('id');
-    console.log("dentro de Libro pequeño clicado has clickado ID=["+id+"]");
+    cargarDatosPrincipales(id);
 
-    $('#path_foto').html('<img src="'+libros[id].path_foto+'">');
-    $('#titulo').html(libros[id].titulo);
-    $('#descripcion').html(libros[id].descripcion);
-    $('#link_ebook').html('<a href="'+libros[id].link_ebook+'">link_ebook</a>');
-    $('#link_comprar').html('<a href="'+libros[id].link_comprar+'">link_comprar</a>');
-    // $('#link_comprar').html(libros[id].link_comprar);
+    // fade in slider_content
+    $(".slider_content").fadeIn();    
+    
+    console.log("dentro de Libro pequeño clicado has clickado ID=["+id+"]");
   });
 });
 
+// function cargarDatosPrincipales
+// carga los datos de slider_content correspondientes al id recibido leidos del json 
+function cargarDatosPrincipales(id){
+  console.log("cargarDatosPrincipales libro=["+id+"]");
+  $('#path_foto').html('<img src="'+libros[id].path_foto+'">');
+  $('#titulo').html(libros[id].titulo);
+  $('#descripcion').html(libros[id].descripcion);
+  $('#link_ebook').html('<a href="'+libros[id].link_ebook+'">link_ebook</a>');
+  $('#link_comprar').html('<a href="'+libros[id].link_comprar+'">link_comprar</a>');
 
+}
 
