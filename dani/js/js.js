8003b8001bfbae07e783fbfd1c9edf16191a7468
Dan <corneliusinlove@gmail.com>
2014-01-12 12:22:37 +0100

    detectando id clicado contstuidos links e imagen

--- dani/js/js.js
+++ dani/js/js.js
@@ -1,36 +1,22 @@
 $(document).ready(function() {
+  // TODO cargar la imagen landing
   console.log("jquery funcionando");
-
+  // TODO borrar el contenido de todas las variables json. Sino el anterior valor sobreescribe un valor vacio
+  
   // Detectar cual libro pequeño ha sido clicado, cambiar el contenido de slider_content
-  $(".change_libros_peq").click(function() {
-    // TODO falta detectar cual id ha sido presionado (ahora uso dummy_id)
-    console.log("dentro de Libro pequeño clicado");
-    var dummy_id="libro1"; // intentando usar variable en "path" json 
-    $('#titulo').html(libros[dummy_id].titulo);
-    $('#descripcion').html(libros[dummy_id].descripcion);
-    $('#link_ebook').html(libros[dummy_id].link_ebook);
-    $('#link_comprar').html(libros[dummy_id].link_comprar);
+  $(".change_libros_peq img").click(function() {
+    // detectar cual id ha sido clicado
+    var id = $(this).attr('id');
+    console.log("dentro de Libro pequeño clicado has clickado ID=["+id+"]");
+
+    $('#path_foto').html('<img src="'+libros[id].path_foto+'">');
+    $('#titulo').html(libros[id].titulo);
+    $('#descripcion').html(libros[id].descripcion);
+    $('#link_ebook').html('<a href="'+libros[id].link_ebook+'">link_ebook</a>');
+    $('#link_comprar').html('<a href="'+libros[id].link_comprar+'">link_comprar</a>');
+    // $('#link_comprar').html(libros[id].link_comprar);
   });
 });
 
 
-var libros=
-{
-  "libro1": {
-    "titulo": "El Francotirador Paciente",
-    "descripcion": "La ciudad es un campo de batalla. Kla sdklfj skldjfasklda klfsd f",
-    "link_ebook": "http://sljasljd.com",
-    "link_comprar": "http://sljasljd.com",
-    "path_foto2": "img/arturo_perez_reverte_el_francotirador_paciente.jpg",
-    "array": [
-      "valor1",
-      "valor2",
-      "valor3",
-      "valor4",
-      "valor5"
-    ]
-  },
-  "libro2": {
-    "titulo": "2222222222 SD asd FASDf sD fASDfas"
-  }
-}
+
---
+++ dani/js/json.js
@@ -0,0 +1,31 @@
+var libros=
+{
+  "libro1": {
+    "titulo": "El Francotirador Paciente",
+    "descripcion": "La ciudad es un campo de batalla. Kla sdklfj skldjfasklda klfsd f",
+    "link_ebook": "http://link_ebook_libro1.com",
+    "link_comprar": "http://link_comprar_libro1.com",
+    "path_foto": "img/arturo_perez_reverte_el_francotirador_paciente.jpg",
+    "array": [
+      "valor1",
+      "valor2",
+      "valor3",
+      "valor4",
+      "valor5"
+    ]
+  },
+  "libro2": {
+    "titulo": "Ad consequat mollit",
+    "descripcion": "2222S Lorem ipsum Ad consequat mollit proident veniam Ut quis aute sed sit ullamco commodo occaecat eiusmod commodo in exercitation.",
+    "link_ebook": "http://link_ebook_libro2.com",
+    "link_comprar": "http://link_comprar_libro2.com",
+    "path_foto": "img/juil_dyler_la_unidad_nilog_al_sima_hongo_ajunjen.jpg",
+    "array": [
+      "valor1",
+      "valor2",
+      "valor3",
+      "valor4",
+      "valor5"
+    ]
+  }
+}
