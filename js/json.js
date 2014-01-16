133df01a19a583472d8ee62b044c0d42392377c8
Dan <corneliusinlove@gmail.com>
2014-01-16 03:58:13 +0100

    botones VER MAS en su sitio y rellenos por contenido del json

--- js/json.js
+++ js/json.js
@@ -1,73 +1,61 @@
 var libros=
 {
   "libro1": {
-    "publicado":"si",
+    "path_img": "img/book1g.png",
     "autor" : "Ray Loriga",
     "titulo": "Za Za, Emperador de Ibiza",
     "descripcion": "Un ex narcotraficante retirado de poca monta que se dedica a la vida contemplativa en Ibiza y cuyas únicas ocupaciones son pasear por la isla y leer la prensa deportiva, se verá metido en una extravagante trama de mafias que lo confunden con el creador de una nueva y poderosísima droga, lo que provocará una espiral de acontecimientos delirantes.",
     "link_ebook": "http://link_ebook_libro1.com",
     "link_comprar": "http://link_comprar_libro1.com",
-    "path_img": "img/book1g.png",
     "cajas":[
-      {"caja_roja":{"texto":"«Abrir 1111 las puertas para salir a jugar» es lo que propone este álbum-almanaque que constituye una nueva aproximación al escritor y al hombre. Un libro que es a su manera muchos libros y que puede leerse sobre todo de dos modos: en la forma corriente (de la A a la Z) o de manera salteada, siguiendo la espiral de la curiosidad y del AZar.", "link_ver_mas":"http://link_linkver_mas_libro11.com"}},
+      {"caja_roja":{"texto":"«1112Abrir 1111 las puertas para salir a jugar» es lo que propone este álbum-almanaque que constituye una nueva aproximación al escritor y al hombre. Un libro que es a su manera muchos libros y que puede leerse sobre todo de dos modos: en la forma corriente (de la A a la Z) o de manera salteada, siguiendo la espiral de la curiosidad y del AZar.", "link_ver_mas":"http://link_linkver_mas_libro11.com"}},
       {"caja_azul":{"texto":"«Abrir  2222 las puertas para salir a jugar» es lo que propone este álbum-almanaque que constituye una nueva aproximación al escritor y al hombre. Un libro que es a su manera muchos libros y que puede leerse sobre todo de dos modos: en la forma corriente (de la A a la Z) o de manera salteada, siguiendo la espiral de la curiosidad y del AZar.", "link_ver_mas":"http://link_linkver_mas_libro1.com2f4g2"}},
-      {"caja_lima":{"texto":"3333s3ñakfsdñ", "link_ver_mas":"toe"}},
-      {"caja_rosa":{"texto":"ROSA3ñakfsdñ", "link_ver_mas":"toe"}},
-      {"caja_verde":{"texto":"VERDEf2f2f", "link_ver_mas":"toe"}},
-      {"caja_naranja":{"texto":"3333s3ñakfsdñ", "link_ver_mas":"toe"}},
-      {"videos":{"text":"4444sñakfsdñ", "link_ver_mas":"toe"}}
+      {"caja_lima":{"texto":"3333s3ñakfsdñ", "link_ver_mas":"http://link_ver_mas.toe"}},
+      {"caja_rosa":{"texto":"ROSA3ñakfsdñ", "link_ver_mas":"http://link_ver_mas.toe"}},
+      {"caja_verde":{"texto":"VERDEf2f2f", "link_ver_mas":"http://link_ver_mas.toe"}},
+      // 3r32
+      {"caja_naranja":{"texto":"3333s3ñakfsdñ", "link_ver_mas":"http://link_ver_mas.toe"}},
 
     ],
-    "array": [
-      "valor1",
-      "valor2",
-      "valor3",
-      "valor4",
-      "valor5"
-    ]
+
   },
   "libro2": {
-    "publicado":"si",
+    "path_img": "img/book2g.png",
     "autor" : "Julio Cortázar, Carles Álvarez Garriga, Aurora Bernárdez",
     "titulo": "Cortázar de la A a la Z",
     "descripcion": "Con motivo del centenario de su nacimiento, la Internacional Cronopia reclamaba ya con demasiada insistencia una nueva aproximación al escritor y al hombre.",
     "link_ebook": "http://link_ebook_libro2.com",
     "link_comprar": "http://link_comprar_libro2.com",
-    "path_img": "img/book2g.png",
     "cajas":[
-      {"caja_roja":{"texto":"«Abrir 1111 las puertas para salir a jugar» es lo que propone este álbum-almanaque que constituye una nueva aproximación al escritor y al hombre. Un libro que es a su manera muchos libros y que puede leerse sobre todo de dos modos: en la forma corriente (de la A a la Z) o de manera salteada, siguiendo la espiral de la curiosidad y del AZar.", "link_ver_mas":"http://link_linkver_mas_libro11.com"}},
-      {"caja_azul":{"texto":"«Abrir  2222 las puertas para salir a jugar» es lo que propone este álbum-almanaque que constituye una nueva aproximación al escritor y al hombre. Un libro que es a su manera muchos libros y que puede leerse sobre todo de dos modos: en la forma corriente (de la A a la Z) o de manera salteada, siguiendo la espiral de la curiosidad y del AZar.", "link_ver_mas":"http://link_linkver_mas_libro1.com2f4g2"}},
-      {"caja_lima":{"texto":"3333s3ñakfsdñ", "link_ver_mas":"toe"}},
-      {"caja_rosa":{"texto":"ROSA3ñakfsdñ", "link_ver_mas":"toe"}},
-      {"caja_verde":{"texto":"VERDEf2f2f", "link_ver_mas":"toe"}},
-      {"caja_naranja":{"texto":"3333s3ñakfsdñ", "link_ver_mas":"toe"}},
-      {"videos":{"text":"4444sñakfsdñ", "link_ver_mas":"toe"}}
+      {"caja_roja":{"texto":"«es modos: en la forma corriente (de la A a la Z) o de manera salteada, siguiendo la espiral de la curiosidad y del AZar.", "link_ver_mas":"http://link_linkver_mas_libro11.com"}},
+      {"caja_azul":{"texto":"«qu leerse sobre todo de dos modos: en la forma corriente (de la A a la Z) o de manera salteada, siguiendo la espiral de la curiosidad y del AZar.", "link_ver_mas":"http://link_linkver_mas_libro1.com2f4g2"}},
+      {"caja_lima":{"texto":"3333s3ñakfsdñ", "link_ver_mas":"http://link_ver_mas.toe"}},
+      {"caja_rosa":{"texto":"ROSA3ñakfsdñ", "link_ver_mas":"http://link_ver_mas.toe"}},
+      {"caja_verde":{"texto":"VERDEf2f2f", "link_ver_mas":"http://link_ver_mas.toe"}},
+      {"caja_naranja":{"texto":"3333s3ñakfsdñ", "link_ver_mas":"http://link_ver_mas.toe"}},
 
     ],
 
-    "array": [
-      "valor1",
-      "valor2",
-      "valor3",
-      "valor4",
-      "valor5"
-    ]
+
   },
   "libro3": {
-    "publicado":"si",
-    "autor" : "Arturo Pé3333333",
-    "titulo": "Nisi exercitation repr",
-    "descripcion": "3333S Lorem ipsum Nisi exercitation reprehenderit consequat.Lorem ipsum Nisi exercitation reprehenderit consequat.Lorem ipsum Nisi exercitation reprehenderit consequat.",
-    "link_ebook": "http://link_ebook_libro3.com",
-    "link_comprar": "http://link_comprar_libro3.com",
     "path_img": "img/book3g.png",
-    "array": [
-      "valor1",
-      "valor2",
-      "valor3",
-      "valor4",
-      "valor5"
-    ]
-  }
+    "autor" : "Julio Cortázar, Carles Álvarez Garriga, Aurora Bernárdez",
+    "titulo": "Cortázar de la A a la Z",
+    "descripcion": "Con motivo del centenario de su nacimimasiada insistencia una nueva aproximación al escritor y al hombre.",
+    "link_ebook": "http://link_ebook_libro2.com",
+    "link_comprar": "http://link_comprar_libro2.com",
+    "cajas":[
+      {"caja_roja":{"texto":"«2222222A", "link_ver_mas":"http://link_linkver_mas_libr24t4t2t4co11.com"}},
+      {"caja_azul":{"texto":"«Abrir  2.", "link_ver_mas":"http://link_linkver_mas_libr23fd32do1.com2f4g2"}},
+      {"caja_lima":{"texto":"3333s3ñakfsdñ", "link_ver_mas":"http://link_ver_mas.toe"}},
+      {"caja_rosa":{"texto":"ROSA3ñakfsdñ", "link_ver_mas":"http://link_ver_mas.toe"}},
+      {"caja_verde":{"texto":"VERDEf2f2f", "link_ver_mas":"http://link_ver_mas.toe"}},
+      {"caja_naranja":{"texto":"3333s3ñakfsdñ", "link_ver_mas":"http://link_ver_mas.toe"}},
+
+    ],
+
+
+  },
 }
 
