d1356f57de570afe934b5fb8503934e972b4ef0d
Dan <corneliusinlove@gmail.com>
2014-01-29 00:18:40 +0100

    codigo arrejuntado (fallo en la url ahora TIENE que ser 'examples' en vez de 'php'

--- php/index.php
+++ php/index.php
@@ -77,36 +77,40 @@ $naitik = $facebook->api('/naitik');
     <h1>php-sdk</h1>
 
     <?php if ($user): ?>
-      <a href="<?php echo $logoutUrl; ?>">Logout</a>
+
+    <h1>HAY USER</h1>
+
+      
     <?php else: ?>
+    <h1>NO HAY USER</h1>
+
+    <?php endif ?>
+      <a href="<?php echo $logoutUrl; ?>">Logout</a>
+      <h3>You</h3>
+      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+
+      <h3>Your User Object (/me)</h3>
+
+      <pre><?php print_r($user_profile); 
+             echo $user_profile['first_name'];
+         echo $user_profile['last_name'];     
+      ?></pre>
+
       <div>
         Check the login status using OAuth 2.0 handled by the PHP SDK:
-        <a href="<?php echo $statusUrl; ?>">Check the login status</a>
+        <a href="<?php echo $statusUrl; ?>">Check the login status<?php echo $statusUrl; ?></a>
       </div>
       <div>
         Login using OAuth 2.0 handled by the PHP SDK:
         <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
       </div>
-    <?php endif ?>
+      <strong><em>You are not Connected.</em></strong>
 
     <h3>PHP Session</h3>
     <pre><?php print_r($_SESSION); ?></pre>
 
-    <?php if ($user): ?>
-      <h3>You</h3>
-      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
-
-      <h3>Your User Object (/me)</h3>
-      <pre><?php print_r($user_profile); 
-      			 echo $user_profile['first_name'];
-				 echo $user_profile['last_name'];     
-      ?></pre>
-    <?php else: ?>
-      <strong><em>You are not Connected.</em></strong>
-    <?php endif ?>
-
-    <h3>Public profile of Naitik</h3>
-    <img src="https://graph.facebook.com/naitik/picture">
-    <?php echo $naitik['name']; ?>
   </body>
 </html>
+<!--
+ https://www.webuzz.es/aplicaciones/skeetching/php/ 
+ -->
