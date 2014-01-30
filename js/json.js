58c82c8691fcfed7d07cfe621562dcb836559895
Dan <corneliusinlove@gmail.com>
2014-01-30 21:06:24 +0100

    .gitignore para no enviar deploy, json.js preparado para open modal

--- js/json.js
+++ js/json.js
@@ -7,7 +7,7 @@ var libros=
     "titulo": "Cortázar de la A a la Z",
     "descripcion": "Abrir las puertas para salir a jugar» es lo que propone este álbum-almanaque que constituye una nueva aproximación al escritor y al hombre. Un libro que es a su manera muchos libros y que puede leerse sobre todo de dos modos: en la forma corriente (de la A a la Z) o de manera salteada, siguiendo la espiral de la curiosidad y del AZar.",
     //"link_ebook": "http://link_ebook_libro1.com",
-    "link_comprar": "https://www.facebook.com/groups/clubdelecturacortazar/",
+    "link_comprar": "#openModal",
     "cajas":[
       {"caja_roja":{"texto":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "link_ver_mas":"https://www.facebook.com/notes/club-de-lectura-cort%C3%A1zar/biograf%C3%ADa-de-julio-cort%C3%A1zar/394690690666079"}},
       {"caja_azul":{"texto":"", "link_ver_mas":"http://link_linkver_mas_libro1.com2f4g2"}},
@@ -27,7 +27,7 @@ var libros=
     "titulo": "Za Za, Emperador de Ibiza",
     "descripcion": "Un ex narcotraficante retirado de poca monta que se dedica a la vida contemplativa en Ibiza y cuyas únicas ocupaciones son pasear por la isla y leer la prensa deportiva, se verá metido en una extravagante trama de mafias que lo confunden con el creador de una nueva y poderosísima droga, lo que provocará una espiral de acontecimientos delirantes.",
     //"link_ebook": "http://link_ebook_libro2.com",
-    "link_comprar": "http://link_comprar_libro2.com",
+    "link_comprar": "#openModal",
     "cajas":[
       {"caja_roja":{"texto":"2 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "link_ver_mas":"http://link_linkver_mas_libro11.com"}},
       {"caja_azul":{"texto":"", "link_ver_mas":"http://link_linkver_mas_libro1.com2f4g2"}},
@@ -46,7 +46,7 @@ var libros=
     "titulo": "La rubia de ojos negros",
     "descripcion": "Lorem ipsum Nisi exercitation reprehenderit consequat.Lorem ipsum Nisi exercitation reprehenderit consequat.Lorem ipsum Nisi exercitation reprehenderit consequat.",
     //"link_ebook": "http://link_ebook_libro3.com",
-    "link_comprar": "http://link_comprar_libro3.com",
+    "link_comprar": "#openModal",
     "cajas":[
       {"caja_roja":{"texto":"3 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "link_ver_mas":"http://link_linkver_mas_libr24t4t2t4co11.com"}},
       {"caja_azul":{"texto":"", "link_ver_mas":"http://link_linkver_mas_libr23fd32do1.com2f4g2"}},
